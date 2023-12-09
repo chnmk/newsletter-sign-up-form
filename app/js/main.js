9895b24f8b8bcb410e5455f5b9e275f707ccfc46
chnmk <a2molchanov@ya.ru>
2023-12-09 14:41:18 +0300

    style: add popup window styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,17 @@
-console.log("Hello World!")
-console.log("Hello World 2!")
+function submitEmail() {
+    if (true) {
+        const mainPage = document.getElementsByClassName("main-page")[0]
+        const popupWindow = document.getElementsByClassName("pop-up")[0]
+        mainPage.classList.add("main-page--disabled");
+        popupWindow.classList.remove("pop-up--disabled")
+    }
+} 
+
+function closePopup() {
+    if (true) {
+        const mainPage = document.getElementsByClassName("main-page")[0]
+        const popupWindow = document.getElementsByClassName("pop-up")[0]
+        popupWindow.classList.add("pop-up--disabled")
+        mainPage.classList.remove("main-page--disabled");
+    }
+} 
