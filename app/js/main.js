7a5ca5bc0730323e9c85e95cd5c3bcdcdeb26b7e
chnmk <a2molchanov@ya.ru>
2024-05-14 10:38:26 +0300

    style: add opacity transition on submit

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,20 @@
 //https://www.w3resource.com/javascript/form/email-validation.php
 const emailValidationRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-function submitEmail() {
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+async function submitEmail() {
     const inputText = document.getElementsByClassName("email-input__input-field")[0].value
     if (emailValidationRegex.test(inputText)) {
         const mainPage = document.getElementsByClassName("main-page")[0]
         const popupWindow = document.getElementsByClassName("pop-up")[0]
         const errorMessage = document.getElementsByClassName("email-input__error")[0]
         const inputField = document.getElementsByClassName("email-input__input-field")[0]
+        mainPage.classList.add("main-page--invisible");
+        await delay(150);
         mainPage.classList.add("main-page--disabled");
         popupWindow.classList.remove("pop-up--disabled")
+        popupWindow.classList.remove("pop-up--invisible");
         errorMessage.classList.add("email-input__error--disabled")
         inputField.classList.remove("email-input__input-field--error")
         document.getElementsByClassName("pop-up__email-display")[0].innerHTML = inputText
@@ -28,9 +33,12 @@ function submitEmail() {
     }
 } 
 
-function closePopup() {
+async function closePopup() {
     const mainPage = document.getElementsByClassName("main-page")[0]
     const popupWindow = document.getElementsByClassName("pop-up")[0]
+    popupWindow.classList.add("pop-up--invisible")
+    await delay(150);
     popupWindow.classList.add("pop-up--disabled")
     mainPage.classList.remove("main-page--disabled");
+    mainPage.classList.remove("main-page--invisible");
 } 
